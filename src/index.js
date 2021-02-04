c270c192166b45413b36d5575697f6f0fcdde925
bibixx <bartosz@legiec.io>
2021-02-04 22:36:48 +0100

    Add sentry io support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 const chalk = require('chalk');
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+require('dotenv').config()
 
 const { goToMainPage } = require('./goToMainPage');
 const { crawlSubject } = require('./crawlSubject');
@@ -10,110 +13,127 @@ const { logger } = require('./logger');
 const { write } = require('./write');
 const { skip } = require('./skip');
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const updatedAt = Date.now();
+Sentry.init({
+  tracesSampleRate: 1.0,
+});
 
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
-  await goToMainPage(page);
-  const subjects = await page.$$eval('table .pyt .cie', ($subjects) => $subjects.map(($s) => $s.value));
-  await page.close();
+const transaction = Sentry.startTransaction({
+  op: "start",
+  name: "Full scrape",
+});
 
-  const subjectsData = {};
-  let retries = 0;
+try {
+  (async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const updatedAt = Date.now();
 
-  for (let i = 0; i < subjects.length;) {
-    const log = logger(subjects, i);
-    const subject = subjects[i];
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await goToMainPage(page);
+    const subjects = await page.$$eval('table .pyt .cie', ($subjects) => $subjects.map(($s) => $s.value));
+    await page.close();
 
-    try {
-      if (skip.includes(subject)) {
-        log('SKIPPING', subject);
+    const subjectsData = {};
+    let retries = 0;
 
-        retries = 0;
-        i++;
-        continue;
-      }
+    for (let i = 0; i < subjects.length;) {
+      const log = logger(subjects, i);
+      const subject = subjects[i];
 
-      log('STARTED', subject, chalk.cyan.bold);
-      const questions = await crawlSubject(browser, i);
+      try {
+        if (skip.includes(subject)) {
+          log('SKIPPING', subject);
 
-      log('QUESTIONS FETCHED', `${subject} (${questions.length})`, chalk.green);
-
-      const numberOfComments = questions.reduce((acc, curr) => curr.comments !== null ? acc + 1 : acc, 0);
-      let commentsFetched = 0;
-
-      log('STARTED COMMENTS', `${subject} (${numberOfComments})`, chalk.magentaBright.bold);
-      for (const question of questions) {
-        const { comments: commentId } = question;
-
-        if (commentId === null) {
+          retries = 0;
+          i++;
           continue;
         }
 
-        const commentsProgress = `${++commentsFetched} / ${numberOfComments}`
+        log('STARTED', subject, chalk.cyan.bold);
+        const questions = await crawlSubject(browser, i);
 
-        log('COMMENT STARTED', `${subject}.${commentId} (${commentsProgress})`, chalk.magentaBright);
-        const comments = await crawlComment(browser, commentId);
-        log('COMMENT FETCHED', `${subject}.${commentId} (${commentsProgress})`, chalk.magentaBright);
+        log('QUESTIONS FETCHED', `${subject} (${questions.length})`, chalk.green);
 
-        question.comments = comments;
-      }
+        const numberOfComments = questions.reduce((acc, curr) => curr.comments !== null ? acc + 1 : acc, 0);
+        let commentsFetched = 0;
 
-      log('FINISHED COMMENTS', `${subject} (${numberOfComments})`, chalk.green);
-      log('FINISHED', subject, chalk.green.bold);
+        log('STARTED COMMENTS', `${subject} (${numberOfComments})`, chalk.magentaBright.bold);
+        for (const question of questions) {
+          const { comments: commentId } = question;
 
-      if (questions.length > 0) {
-        const id = getId(subject);
+          if (commentId === null) {
+            continue;
+          }
 
-        const data = {
-          title: subject,
-          id,
-          data: questions,
-          updatedAt,
+          const commentsProgress = `${++commentsFetched} / ${numberOfComments}`
+
+          log('COMMENT STARTED', `${subject}.${commentId} (${commentsProgress})`, chalk.magentaBright);
+          const comments = await crawlComment(browser, commentId);
+          log('COMMENT FETCHED', `${subject}.${commentId} (${commentsProgress})`, chalk.magentaBright);
+
+          question.comments = comments;
         }
 
-        subjectsData[id] = questions;
+        log('FINISHED COMMENTS', `${subject} (${numberOfComments})`, chalk.green);
+        log('FINISHED', subject, chalk.green.bold);
 
-        await write(
-          path.join(__dirname, '..', 'out', `${data.id}.json`),
-          JSON.stringify(data, null, 2),
-        );
-      } else {
-        log('SKIPPING EMPTY', subject, chalk.bold);
-      }
+        if (questions.length > 0) {
+          const id = getId(subject);
 
-      retries = 0;
-      i++;
-    } catch (error) {
-      if (retries < 4) {
-        log('RETRY', `${subject}`, chalk.yellow.bold);
-        console.log(chalk.gray(error.stack));
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        retries++;
-      } else {
-        log('ERROR', `Skipping ${subject}`, chalk.red.bold);
-        console.error(chalk.red(error.stack));
+          const data = {
+            title: subject,
+            id,
+            data: questions,
+            updatedAt,
+          }
+
+          subjectsData[id] = questions;
+
+          await write(
+            path.join(__dirname, '..', 'out', `${data.id}.json`),
+            JSON.stringify(data, null, 2),
+          );
+        } else {
+          log('SKIPPING EMPTY', subject, chalk.bold);
+        }
+
+        retries = 0;
         i++;
+      } catch (subjectError) {
+        if (retries < 4) {
+          log('RETRY', `${subject}`, chalk.yellow.bold);
+          console.log(chalk.gray(subjectError.stack));
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          retries++;
+        } else {
+          Sentry.captureException(subjectError);
+          log('ERROR', `Skipping ${subject}`, chalk.red.bold);
+          console.error(chalk.red(subjectError.stack));
+          i++;
+        }
       }
     }
-  }
 
-  const indexData = {
-    updatedAt,
-    pages: subjects
-      .filter((subject) => subjectsData[getId(subject)]?.length > 0)
-      .map((subject) => ({
-        title: subject,
-        id: getId(subject)
-      }))
-  }
+    const indexData = {
+      updatedAt,
+      pages: subjects
+        .filter((subject) => subjectsData[getId(subject)]?.length > 0)
+        .map((subject) => ({
+          title: subject,
+          id: getId(subject)
+        }))
+    }
 
-  await write(
-    path.join(__dirname, '..', 'out', `index.json`),
-    JSON.stringify(indexData, null, 2),
-  );
+    await write(
+      path.join(__dirname, '..', 'out', `index.json`),
+      JSON.stringify(indexData, null, 2),
+    );
 
-  browser.close();
-})();
+    browser.close();
+  })();
+} catch (error) {
+  Sentry.captureException(error);
+  console.error(chalk.red(error.stack));
+} finally {
+  transaction.finish();
+}
